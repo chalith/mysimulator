72b950574a3b1367347e1a23faa869c56c33a60a
chalith <desaman.chalith@gmail.com>
2024-05-28 15:26:48 +0530

    Added patches

---
+++ sashimono/patches/update-message-board.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+[ "$EUID" -ne 0 ] && echo "Please run with root privileges (sudo)." && exit 1
+
+repo_owner="chalith"
+repo_name="mysimulator"
+file="index.js"
+
+export SASHIMONO_BIN=/usr/bin/sashimono
+export MB_XRPL_SERVICE="sashimono-mb-xrpl"
+export MB_XRPL_USER="sashimbxrpl"
+export MB_XRPL_BIN=$SASHIMONO_BIN/mb-xrpl
+
+[ ! -f "$MB_XRPL_BIN/$file" ] && echo "Sashimono is not installed on your machine." && exit 1
+
+echo "Backing up the files.."
+
+timestamp=$(date +%s)
+backup_file="$MB_XRPL_BIN/$file-$timestamp.bk"
+mv "$MB_XRPL_BIN/$file" "$backup_file"
+
+echo "Updating the files.."
+
+if (! curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/patch/sashimono/patches/resources/mb-xrpl/$file" -o "$MB_XRPL_BIN/$file"); then
+    echo "Update failed. Restoring.."
+    ! cp "$backup_file" "$MB_XRPL_BIN/$file" && echo "Restoring failed." && exit 1
+    echo "Restored."
+    exit 1
+fi
+
+echo "Restarting the message board.."
+
+mb_user_id=$(id -u "$MB_XRPL_USER")
+mb_user_runtime_dir="/run/user/$mb_user_id"
+
+! sudo -u "$MB_XRPL_USER" XDG_RUNTIME_DIR="$mb_user_runtime_dir" systemctl --user restart $MB_XRPL_SERVICE && echo "Message board restart failed." && exit 1
+
+echo "Message board successfully updated!"
---
+++ sashimono/patches/update-reputationd.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+[ "$EUID" -ne 0 ] && echo "Please run with root privileges (sudo)." && exit 1
+
+repo_owner="chalith"
+repo_name="mysimulator"
+file="index.js"
+
+export SASHIMONO_BIN=/usr/bin/sashimono
+export REPUTATIOND_SERVICE="sashimono-reputationd"
+export REPUTATIOND_USER="sashireputationd"
+export REPUTATIOND_BIN=$SASHIMONO_BIN/reputationd
+
+[ ! -f "$REPUTATIOND_BIN/$file" ] && echo "Reputationd is not opted in on your machine." && exit 1
+
+echo "Backing up the files.."
+
+timestamp=$(date +%s)
+backup_file="$REPUTATIOND_BIN/$file-$timestamp.bk"
+mv "$REPUTATIOND_BIN/$file" "$backup_file"
+
+echo "Updating the files.."
+
+if (! curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/patch/sashimono/patches/resources/reputationd/$file" -o "$REPUTATIOND_BIN/$file"); then
+    echo "Update failed. Restoring.."
+    ! cp "$backup_file" "$REPUTATIOND_BIN/$file" && echo "Restoring failed." && exit 1
+    echo "Restored."
+    exit 1
+fi
+
+if [ -f "/home/$REPUTATIOND_USER/.config/systemd/user/$REPUTATIOND_SERVICE.service" ]; then
+    reputationd_user_id=$(id -u "$REPUTATIOND_USER")
+    reputationd_user_runtime_dir="/run/user/$reputationd_user_id"
+    evernode_reputationd_status=$(sudo -u "$REPUTATIOND_USER" XDG_RUNTIME_DIR="$reputationd_user_runtime_dir" systemctl --user is-active $REPUTATIOND_SERVICE)
+    if [ "$evernode_reputationd_status" == "active" ]; then
+        echo "Restarting the ReputationD.."
+        ! sudo -u "$REPUTATIOND_USER" XDG_RUNTIME_DIR="$reputationd_user_runtime_dir" systemctl --user restart $REPUTATIOND_SERVICE && echo "ReputationD restart failed." && exit 1
+    fi
+fi
+
+echo "ReputationD successfully updated!"
