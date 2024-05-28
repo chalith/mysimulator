791b614e4a99618a7808d56cf30dffc2223ab494
chalith <desaman.chalith@gmail.com>
2024-05-28 15:37:16 +0530

    Permission update

--- sashimono/patches/update-message-board.sh
+++ sashimono/patches/update-message-board.sh
@@ -21,7 +21,7 @@ mv "$MB_XRPL_BIN/$file" "$backup_file"
 
 echo "Updating the files.."
 
-if (! curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/patch/sashimono/patches/resources/mb-xrpl/$file" -o "$MB_XRPL_BIN/$file"); then
+if (! curl "https://raw.githubusercontent.com/$repo_owner/$repo_name/patch/sashimono/patches/resources/mb-xrpl/$file" -o "$MB_XRPL_BIN/$file") && chmod +x "$MB_XRPL_BIN/$file"; then
     echo "Update failed. Restoring.."
     ! cp "$backup_file" "$MB_XRPL_BIN/$file" && echo "Restoring failed." && exit 1
     echo "Restored."
