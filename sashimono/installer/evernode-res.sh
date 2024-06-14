eff69125d9ff247115d77635e658c53f0c3ec145
chalith <desaman.chalith@gmail.com>
2024-06-14 11:23:34 +0530

    Added test setups

--- sashimono/installer/evernode-res.sh
+++ sashimono/installer/evernode-res.sh
@@ -39,4 +39,4 @@ fi
 echo "Found Sashimono $VERSION..."
 
 # Exucute the setup
-curl -fsSL $setup | cat | sudo NETWORK=${NETWORK:-mainnet} bash -s ${@}
+curl -fsSL $setup | cat | sudo NETWORK=${NETWORK:-mainnet} SKIP_SYSREQ=1 NO_DOMAIN=1 bash -s ${@}
