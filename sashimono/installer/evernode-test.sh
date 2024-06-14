64e904ce4962ad0bdd7d172f2347da5c53e77852
chalith <desaman.chalith@gmail.com>
2024-06-14 10:58:30 +0530

    Added new governor

--- sashimono/installer/evernode-test.sh
+++ sashimono/installer/evernode-test.sh
@@ -39,4 +39,4 @@ fi
 echo "Found Sashimono $VERSION..."
 
 # Exucute the setup
-curl -fsSL $setup | cat | sudo NETWORK=devnet OVERRIDE_EVERNODE_GOVERNOR_ADDRESS=r9DFqbCVyNkA5HBog1hME9iSdg9FnbXgnV SKIP_SYSREQ=1 NO_DOMAIN=1 bash -s ${@}
+curl -fsSL $setup | cat | sudo NETWORK=devnet OVERRIDE_EVERNODE_GOVERNOR_ADDRESS=rwDEhKkChUFpZ9Tmtq7e41LMFuvFwirHmf SKIP_SYSREQ=1 NO_DOMAIN=1 bash -s ${@}
